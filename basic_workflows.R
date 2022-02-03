a89cda36ff46c81729273c62a9d65d766325fc63
tiagocmsouza <tiagocmsouza@gmail.com>
2022-02-03 23:04:53 +0000

    Recipes & complete tibble

--- basic_workflows.R
+++ basic_workflows.R
@@ -4,28 +4,28 @@
 library(tidymodels)
 
 # 1 - Linear Regression ----
-workflow_lm <- workflow() %>% 
+lm <- workflow() %>% 
   add_model(
     linear_reg() %>% 
       set_engine("lm")
   )
 
 # 2 - XGBoost ----
-workflow_xgb <- workflow() %>% 
+xgb <- workflow() %>% 
   add_model(
     boost_tree("regression", learn_rate = 0.25) %>% 
       set_engine("xgboost")
   )
 
 # 3 - Random Forest ----
-workflow_rf <- workflow() %>% 
+rf <- workflow() %>% 
   add_model(
     rand_forest() %>% 
       set_engine("ranger")
   )
 
 # 4 - SVM
-workflow_svm <- workflow() %>% 
+svm <- workflow() %>% 
   add_model(
     svm_rbf() %>% 
       set_engine ("kernlab")
